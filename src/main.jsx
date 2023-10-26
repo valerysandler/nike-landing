ed0f4310c322673e6bc61cd222043181857c578e
valerysand <kolomietsval@gmail.com>
2023-10-26 22:23:47 +0300

    firebase connect

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAhcjSu-r7htJBUPqt63WPOxHkmCYZC-vM",
+  authDomain: "nike-landing-e4efd.firebaseapp.com",
+  projectId: "nike-landing-e4efd",
+  storageBucket: "nike-landing-e4efd.appspot.com",
+  messagingSenderId: "764234970174",
+  appId: "1:764234970174:web:047ad771c609a208414d6f",
+  measurementId: "G-VB0RGLSWGS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
